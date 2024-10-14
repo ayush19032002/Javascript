d6c561b7abadd5123398c163cd5dfba9e4297e41
Ayush Raghuvanshi <99779711+ayush19032002@users.noreply.github.com>
2024-10-15 01:47:48 +0530

    Add files via upload

---
+++ operators/index.js
@@ -0,0 +1,30 @@
+let text = "5";
+let text2 = 5;
+
+console.log(text == text2);
+
+// equal value and equal type
+console.log(text === text2);
+
+// not == !
+console.log(text !== text2);
+
+
+// increment of one 
+console.log(text2++);
+
+
+// && => and
+// || => OR 
+// ! => not 
+
+let x = 8;
+let y = 1;
+x += y
+console.log(x);
+
+x -= y;
+console.log(x);
+
+// ternary operator 
+x === 8 ? console.log("good morning") : console.log("Good evening")
