1c7712b35f1526579921154a38a403b87806be9e
Ayush Raghuvanshi <99779711+ayush19032002@users.noreply.github.com>
2024-10-15 01:22:11 +0530

    Add files via upload

---
+++ variable/index.js
@@ -0,0 +1,75 @@
+console.log("Variables");
+
+// let //
+let a=34 + 45;
+console.log(a);
+
+let x = 55
+let y = 45
+let z = x+y
+console.log(z);
+
+let game = "chess";
+console.log(game);
+
+let u = 500
+let v = 400
+console.log(u+v);
+
+
+// block scope //
+{
+    let s = 43;
+    let p = 42;
+    let q = s+p;
+    console.log(q);
+}
+
+{
+    let s = 45
+    let t = 65
+    let e = 55
+    console.log(s+t+e); 
+}
+
+{
+    let abc = 600
+    let cde = 600
+    console.log(abc + cde);
+}
+
+// var //
+var d = 45;
+var e = 60;
+var f = d+e;
+console.log(f);
+
+var bikename = "duke";
+console.log(bikename);
+
+var t = 44
+var t = 43
+console.log(t*t);
+
+// const //
+const j = 78;
+const k = 88;
+const g = j + k;
+console.log(g);
+
+const b = 56;
+const n = 67;
+console.log(b+n);
+
+{
+    const a = 56
+    const h = 50
+    console.log(a+h)
+}
+
+const carname = "volvo";
+console.log(carname);
+
+const ab = 56
+const cd = 65
+console.log(ab+cd);
---
+++ variable/index1.js
@@ -0,0 +1,19 @@
+console.log("variable");
+
+// object
+let person = {username:"aayush", age:"21",surname:"raghu1c",};
+console.log(person);
+console.log(person.username);
+console.log(person.age);
+
+// array
+let multiple = [{username:"aayush", age:"21", surname:"raghu1c"},{username:"payal",age:"20",surname:"soni"}];
+console.log(multiple);
+console.log(multiple.length);
+console.log(multiple[0]);
+console.log(multiple[1]);
+
+
+
+
+
